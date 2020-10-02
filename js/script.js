dc29dfb7843ba0dfea50d839510a10f9a5274981
Christopher <christopherpersinet@gmail.com>
2020-10-02 10:12:04 +0200

    modification du js

---
+++ js/script.js
@@ -0,0 +1,140 @@
+let name=0
+function togglePopup(name){
+  switch (name){
+    case 1:    
+      document.getElementById("popup-1").classList.toggle("active");
+      break
+    case 2:
+      document.getElementById("popup-2").classList.toggle("active");
+      break
+    case 3:
+      document.getElementById("popup-3").classList.toggle("active");
+      break
+    case 4:
+      document.getElementById("popup-4").classList.toggle("active");
+      break
+    case 5:
+      document.getElementById("popup-5").classList.toggle("active");
+      break
+    case 6:
+      document.getElementById("popup-6").classList.toggle("active");
+      break
+    case 7:
+      document.getElementById("popup-7").classList.toggle("active");
+      break
+    case 8:
+      document.getElementById("popup-8").classList.toggle("active");
+      break
+    case 9:
+      document.getElementById("popup-9").classList.toggle("active");
+      break
+    case 10:
+      document.getElementById("popup-10").classList.toggle("active");
+      break
+    case 11:
+      document.getElementById("popup-11").classList.toggle("active");
+      break
+    case 12:
+      document.getElementById("popup-12").classList.toggle("active");
+      break
+    case 13:
+      document.getElementById("popup-13").classList.toggle("active");
+      break
+    case 14:
+      document.getElementById("popup-14").classList.toggle("active");
+      break
+    case 15:
+      document.getElementById("popup-15").classList.toggle("active");
+      break
+    case 16:
+      document.getElementById("popup-16").classList.toggle("active");
+      break
+    case 17:
+      document.getElementById("popup-17").classList.toggle("active");
+      break
+    case 18:
+      document.getElementById("popup-18").classList.toggle("active");
+      break
+    case 19:
+      document.getElementById("popup-19").classList.toggle("active");
+      break
+    case 20:
+      document.getElementById("popup-20").classList.toggle("active");
+      break
+    case 21:
+      document.getElementById("popup-21").classList.toggle("active");
+      break
+    case 22:
+      document.getElementById("popup-22").classList.toggle("active");
+      break
+    case 23:
+      document.getElementById("popup-23").classList.toggle("active");
+      break
+  }
+}
+// function togglePopupAxelC(){
+//   document.getElementById("popup-2").classList.toggle("active");
+// }
+// function togglePopupAxelR(){
+//   document.getElementById("popup-3").classList.toggle("active");
+// }
+// function togglePopupAliceP(){
+//   document.getElementById("popup-4").classList.toggle("active");
+// }
+// function togglePopupAlexC(){
+//   document.getElementById("popup-5").classList.toggle("active");
+// }
+// function togglePopupBaptisteV(){
+//   document.getElementById("popup-6").classList.toggle("active");
+// }
+// function togglePopupAdrienT(){
+//   document.getElementById("popup-7").classList.toggle("active");
+// }
+// function togglePopupBrunoL(){
+//   document.getElementById("popup-8").classList.toggle("active");
+// }
+// function togglePopupChristopheC(){
+//   document.getElementById("popup-9").classList.toggle("active");
+// }
+// function togglePopupChristopherP(){
+//   document.getElementById("popup-10").classList.toggle("active");
+// }
+// function togglePopupCharlieG(){
+//   document.getElementById("popup-11").classList.toggle("active");
+// }
+// function togglePopupDamienL(){
+//   document.getElementById("popup-12").classList.toggle("active");
+// }
+// function togglePopupDenisC(){
+//   document.getElementById("popup-13").classList.toggle("active");
+// }
+// function togglePopupEnzoG(){
+//   document.getElementById("popup-14").classList.toggle("active");
+// }
+// function togglePopupIlyesR(){
+//   document.getElementById("popup-15").classList.toggle("active");
+// }
+// function togglePopupLeandreG(){
+//   document.getElementById("popup-16").classList.toggle("active");
+// }
+// function togglePopupMorganeJ(){
+//   document.getElementById("popup-17").classList.toggle("active");
+// }
+// function togglePopupMassinthaAB(){
+//   document.getElementById("popup-18").classList.toggle("active");
+// }
+// function togglePopupNathanM(){
+//   document.getElementById("popup-19").classList.toggle("active");
+// }
+// function togglePopupOlivierP(){
+//   document.getElementById("popup-20").classList.toggle("active");
+// }
+// function togglePopupSteveC(){
+//   document.getElementById("popup-21").classList.toggle("active");
+// }
+// function togglePopupThomasG(){
+//   document.getElementById("popup-22").classList.toggle("active");
+// }
+// function togglePopupVictorB(){
+//   document.getElementById("popup-23").classList.toggle("active");
+// }
